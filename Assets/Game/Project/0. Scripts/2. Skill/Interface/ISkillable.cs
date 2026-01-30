2f2b0cc690b30d0f164aee87aa19f34406969666
elentenco96 <elentecno96@gmail.com>
2026-01-30 13:30:08 +0900

    파일 위치 재정의

--- Assets/Game/Project/0. Scripts/2. Skill/Interface/ISkillable.cs
+++ Assets/Game/Project/0. Scripts/2. Skill/Interface/ISkillable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Game.Project._0.Scripts._3.Skill.Interface
+namespace Game.Project._0.Scripts._2.Skill.Interface
 {
     public interface ISkillable
     {
